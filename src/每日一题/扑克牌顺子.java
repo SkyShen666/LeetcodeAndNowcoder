3064a264c096711d6ca84e02f24f2fb71e38125a
Shen <543190701@qq.com>
2020-07-02 18:51:18 +0800

    剑指Offer45

--- src/每日一题/扑克牌顺子.java
+++ src/每日一题/扑克牌顺子.java
@@ -3,6 +3,7 @@ package 每日一题;
 import java.util.Arrays;
 
 /**
+ * JZ45
  * LL今天心情特别好,因为他去买了一副扑克牌,发现里面居然有2个大王,2个小王(一副牌原本是54张^_^)...
  * 他随机从中抽出了5张牌,想测测自己的手气,看看能不能抽到顺子,如果抽到的话,他决定去买体育彩票,嘿嘿！！
  * “红心A,黑桃3,小王,大王,方片5”,“Oh My God!”不是顺子.....LL不高兴了,他想了想,
@@ -13,28 +14,35 @@ import java.util.Arrays;
  */
 public class 扑克牌顺子 {
     public boolean isContinuous(int [] numbers) {
-        if (numbers == null || numbers.length < 1)
+        if(numbers.length < 5 || numbers == null) {
             return false;
-        Arrays.sort(numbers);
-        int numberOfZero = 0;
-        int numberOfGap = 0;
-
-        //统计0的个数
-        for (int i = 0; i < numbers.length && numbers[i] == 0; i++)
-            numberOfZero++;
-
-
-        //统计间隔的数目
-        int small = numberOfZero;
-        int big = small + 1;
-        while (big < numbers.length) {
-            //出现了对子，肯定不是顺子了
-            if (numbers[small] == numbers[big])
-                return false;
-            numberOfGap += numbers[big] - numbers[small] - 1;
-            small++;
-            big++;
         }
-        return numberOfGap - numberOfZero > 0 ? false : true;
+        Arrays.sort(numbers);
+
+        // 统计0的个数（0为万能数）
+        int zeroCount = 0;
+        for(int i = 0; i < 5 && numbers[i] == 0; i++) {
+            zeroCount++;
+        }
+
+        // 遍历
+        for(int i = zeroCount + 1; i < 5; i++) {
+            // 出现对子
+            if(numbers[i] == numbers[i - 1]) {
+                return false;
+            } else if(numbers[i] - numbers[i - 1] == 1){
+                continue;
+            } else {
+                if(zeroCount == 0) {
+                    return false;
+                }
+                int gapCount = numbers[i] - numbers[i - 1] - 1;
+                zeroCount -= gapCount;
+                if(zeroCount < 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
