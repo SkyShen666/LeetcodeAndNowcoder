fc7bde4cf4ed55166fcd6f8e023c930e6da01919
Shen <543190701@qq.com>
2020-07-03 00:22:10 +0800

    leetcode105 & JZ4

---
+++ src/树/No_105从前序与中序遍历序列构造二叉树.java
@@ -0,0 +1,30 @@
+package 树;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ */
+import java.util.*;
+public class No_105从前序与中序遍历序列构造二叉树 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[0]);
+        for(int i = 0; i < inorder.length; i++) {
+            if(inorder[i] == root.val) {
+                root.left = buildTree(Arrays.copyOfRange(preorder, 1, i + 1),
+                                        Arrays.copyOfRange(inorder, 0, i));
+                root.right = buildTree(Arrays.copyOfRange(preorder, i + 1, preorder.length),
+                                        Arrays.copyOfRange(inorder, i + 1, inorder.length));
+            }
+        }
+        return root;
+    }
+}
--- src/每日一题/重建二叉树.java
+++ src/每日一题/重建二叉树.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * JZ4
  * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
  * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
  */
@@ -23,7 +24,6 @@ public class 重建二叉树 {
                 root.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i + 1, pre.length),
                                                     Arrays.copyOfRange(in,i + 1, in.length));
                 break;
-
             }
         }
         return root;
