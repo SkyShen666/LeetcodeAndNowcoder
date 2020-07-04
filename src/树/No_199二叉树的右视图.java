191852130f611a6d22b871fc8bf53af027c933cd
Shen <543190701@qq.com>
2020-07-04 23:05:42 +0800

    LeetCode199 二叉树的右视图

--- src/树/No_199二叉树的右视图.java
+++ src/树/No_199二叉树的右视图.java
@@ -6,6 +6,13 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.LinkedList;
 
+/**
+ * 思路： 利用 BFS 进行层次遍历，记录下每层的最后一个元素。
+ * 时间复杂度： O(N)，每个节点都入队出队了 1 次。
+ * 空间复杂度： O(N)，使用了额外的队列空间。
+ *
+ * 链接：https://leetcode-cn.com/problems/binary-tree-right-side-view/solution/jian-dan-bfsdfs-bi-xu-miao-dong-by-sweetiee/
+ */
 public class No_199二叉树的右视图 {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
