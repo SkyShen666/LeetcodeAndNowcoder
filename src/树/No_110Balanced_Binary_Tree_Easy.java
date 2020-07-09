16b404621e3138e562dbc1936d66fcde15e8944d
Shen <543190701@qq.com>
2020-07-09 16:07:17 +0800

    LeetCode110 平衡二叉树

--- src/树/No_110Balanced_Binary_Tree_Easy.java
+++ src/树/No_110Balanced_Binary_Tree_Easy.java
@@ -1,29 +1,48 @@
 package 树;
 
-import jdk.nashorn.api.tree.Tree;
-
+/**
+ * 自底向上
+ * 思路是对二叉树做先序遍历，从底至顶返回子树最大高度，若判定某子树不是平衡树则 “剪枝” ，直接向上返回。
+ *
+ * 算法流程：
+ * recursion(root):
+ *
+ * 递归返回值：
+ * 当节点root 左 / 右子树的高度差 < 2 ：
+ *      则返回以节点root为根节点的子树的最大高度，即节点 root 的左右子树中最大高度加 1（ max(left, right) + 1 ）；
+ * 当节点root 左 / 右子树的高度差 ≥2 ：
+ *      则返回 -1 ，代表此子树不是平衡树 。
+ *
+ * 递归终止条件：
+ * 当越过叶子节点时，返回高度 0；
+ * 当左（右）子树高度 left== -1 时，代表此子树的 左（右）子树 不是平衡树，因此直接返回 -1；
+ * isBalanced(root) ：
+ *
+ * 返回值： 若 recur(root) != 1 ，则说明此树平衡，返回 true； 否则返回false 。
+ *
+ * 复杂度分析：
+ *  时间复杂度 O(N)： N为树的节点数；最差情况下，需要递归遍历树的所有节点。
+ *  空间复杂度 O(N)： 最差情况下（树退化为链表时），系统递归需要使用 O(N)的栈空间。
+ */
 public class No_110Balanced_Binary_Tree_Easy {
-    public static void main(String[] args) {
-
-    }
-
-    private boolean result = true;
 
     public boolean isBalanced(TreeNode root) {
-        maxDepth(root);
-        return result;
+        return recursion(root) != -1;
     }
 
-    private int maxDepth(TreeNode root) {
+    private int recursion(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        if (Math.abs(left - right) > 1) {
-            result = false;
+        int left = recursion(root.left);
+        if (left == -1) {
+            return -1;
         }
-        return Math.max(left, right) + 1;
+        int right = recursion(root.right);
+        if (right == -1) {
+            return -1;
+        }
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
     }
 }
 
