cf40ff6e599441c93645c3eba1f3d7b0425c24a5
Shen <543190701@qq.com>
2020-07-11 10:29:13 +0800

    LeetCode322 零钱兑换

--- src/动态规划/No_322零钱兑换.java
+++ src/动态规划/No_322零钱兑换.java
@@ -4,8 +4,11 @@ import java.util.*;
 
 /**
  * LeetCode 322
- * 动态规划，自底向上
- * f(i) = Min(f(i - coins[j]) + 1)
+ * 动态规划，自底向上 （官方题解，方法三）
+ * 我们采用自下而上的方式进行思考。
+ * 仍定义 F(i)F(i) 为组成金额 ii 所需最少的硬币数量，假设在计算 F(i)F(i) 之前，
+ * 我们已经计算出 F(0)-F(i-1)F(0)−F(i−1) 的答案。 则 F(i)F(i) 对应的转移方程应为
+ * F(i) = Min(F(i - coins[j]) + 1)   (j = 0 ... n-1)
  */
 public class No_322零钱兑换 {
     public int coinChange(int[] coins, int amount) {
