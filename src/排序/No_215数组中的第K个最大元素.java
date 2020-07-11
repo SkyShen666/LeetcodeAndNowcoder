dde9455953fd04339cd3de254b2bfc6b86276632
Shen <543190701@qq.com>
2020-07-11 17:05:45 +0800

    LeetCode215 数组中的第k个最大元素

--- src/排序/No_215数组中的第K个最大元素.java
+++ src/排序/No_215数组中的第K个最大元素.java
@@ -2,6 +2,11 @@ package 排序;
 
 import java.util.PriorityQueue;
 
+/**
+ * 方法一：基于快排
+ * 情况是最坏的，时间代价是 O(n ^ 2)
+ *。我们可以引入随机化来加速这个过程，它的时间代价的期望是 O(n)
+ */
 public class No_215数组中的第K个最大元素 {
     public int findKthLargest(int[] nums, int k) {
         k = nums.length - k;
@@ -9,14 +14,14 @@ public class No_215数组中的第K个最大元素 {
         while (low < high) {
             int pivotPos = partition(nums, low, high);
             if (pivotPos == k) {
-                break;
+                return nums[pivotPos];
             } else if (pivotPos < k) {
                 low = pivotPos + 1;
             } else {
                 high = pivotPos - 1;
             }
         }
-        return nums[k];
+        return nums[low];
     }
 
     private int partition(int[] a, int low, int high) {
