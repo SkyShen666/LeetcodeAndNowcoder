7a5fcb641025083e6e78ec13ed77ed6c8703ba99
Shen <543190701@qq.com>
2020-07-12 22:29:50 +0800

    LeetCode102 二叉树的层序遍历

---
+++ src/树/No_102二叉树的层序遍历.java
@@ -0,0 +1,31 @@
+package 树;
+
+import java.util.*;
+
+public class No_102二叉树的层序遍历 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null) {
+            return lists;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (size > 0) {
+                TreeNode node = queue.remove();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                list.add(node.val);
+                size--;
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+}
