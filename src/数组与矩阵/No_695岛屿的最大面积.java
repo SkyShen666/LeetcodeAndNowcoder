6733ce96330fdd80ccd6b989c3d4bd49bdab86f1
Shen <543190701@qq.com>
2020-07-21 12:20:51 +0800

    刷题鸭

---
+++ src/数组与矩阵/No_695岛屿的最大面积.java
@@ -0,0 +1,28 @@
+package 数组与矩阵;
+
+public class No_695岛屿的最大面积 {
+    public int maxAreaOfIsland(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    res = Math.max(res, dfs(i, j, grid));
+                }
+            }
+        }
+        return res;
+    }
+
+    private int dfs(int i, int j, int[][] grid) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
+        }
+        int num = 1;
+        grid[i][j] = 0;
+        num += dfs(i + 1, j, grid);
+        num += dfs(i - 1, j, grid);
+        num += dfs(i, j + 1, grid);
+        num += dfs(i, j - 1, grid);
+        return num;
+    }
+}
