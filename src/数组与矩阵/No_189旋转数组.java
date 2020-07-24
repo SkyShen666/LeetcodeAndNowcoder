a8f6b52e4a76b8530f21f9d3e1e50150285db08b
Shen <543190701@qq.com>
2020-07-24 14:33:26 +0800

    刷题鸭

--- src/数组与矩阵/No_189旋转数组.java
+++ src/数组与矩阵/No_189旋转数组.java
@@ -17,3 +17,27 @@ public class No_189旋转数组 {
         }
     }
 }
+/**
+ * 三次反转法：（官方题解）
+ public void rotate(int[] nums, int k) {
+ if (nums == null || nums.length == 0 || nums.length == k) {
+ return;
+ }
+ if (nums.length < k) {
+ k = k - nums.length;
+ }
+ reverse(nums, 0, nums.length - 1);
+ reverse(nums, 0, k - 1);
+ reverse(nums, k, nums.length - 1);
+ }
+
+ private void reverse(int[] nums, int i, int j) {
+ while (i < j) {
+ int temp = nums[i];
+ nums[i] = nums[j];
+ nums[j] = temp;
+ i++;
+ j--;
+ }
+ }
+ */
