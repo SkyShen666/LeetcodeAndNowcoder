847ff5c90732879ba58db2648e06bd6a8ff4c189
shenguanchu <543190701@qq.com>
2020-09-16 13:59:02 +0800

    刷题鸭

---
+++ src/动态规划/No_416分割等和子集.java
@@ -0,0 +1,67 @@
+package 动态规划;
+
+public class No_416分割等和子集 {
+    // 优化，状态压缩
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        // 总和为奇数，不可能能划分成两个子集
+        if (sum % 2 != 0) {
+            return false;
+        }
+        sum = sum / 2;
+        int n = nums.length;
+        // 所有只考虑从前i个数中选，是否有一个选法使总和等于j的集合。
+        boolean[] dp = new boolean[sum + 1];
+
+        //base case
+        dp[0] = true;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = sum; j >= nums[i]; j--) {
+                dp[j] = dp[j] | dp[j - nums[i]];
+            }
+        }
+        return dp[sum];
+    }
+}
+
+/**
+ public boolean canPartition(int[] nums) {
+ if (nums == null || nums.length == 0) {
+ return false;
+ }
+ int sum = 0;
+ for (int num : nums) {
+ sum += num;
+ }
+ // 总和为奇数，不可能能划分成两个子集
+ if (sum % 2 != 0) {
+ return false;
+ }
+ sum = sum / 2;
+ int n = nums.length;
+ // 所有只考虑从前i个数中选，是否有一个选法使总和等于j的集合。
+ boolean[][] dp = new boolean[n + 1][sum + 1];
+
+ // base case
+ // j = 0，一个都不选
+ for (int i = 0; i <= n; i++) {
+ dp[i][0] = true;
+ }
+
+ for (int i = 1; i < n; i++) {
+ for (int j = 1; j <= sum; j++) {
+ dp[i][j] = dp[i - 1][j];
+ if (j >= nums[i])
+ dp[i][j] = dp[i][j] | dp[i - 1][j - nums[i]];
+ }
+ }
+ return dp[n - 1][sum];
+ }
+ */
--- src/动态规划/No_518零钱兑换2.java
+++ src/动态规划/No_518零钱兑换2.java
@@ -1,18 +1,52 @@
 package 动态规划;
 
-/**
- * 经典背包问题：参考的官方题解
- */
+
 public class No_518零钱兑换2 {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-        // 这步很关键，凑零元，一个硬币都不需要，此方案唯一，方案数量为1
-        dp[0] = 1;
-        for (int i = 0; i < coins.length; i++) {
-            for(int j = coins[i]; j <= amount; j++) {
-                dp[j] = dp[j] + dp[j - coins[i]];
-            }
+        int n = coins.length;
+        if (n == 0) {
+            return amount == 0 ? 1 : 0;
         }
-        return dp[amount];
+
+        int[][] dp = new int[n][amount + 1];
+        // base case
+        // 总金额 = 0，方案只有一个：所有硬币拿0个
+        dp[0][0] = 1;
+        // 初始化第一行,只考虑用第一种硬币。（为了下面计算数组不越界）
+        for (int i = coins[0]; i <= amount; i += coins[0])
+            dp[0][i] = 1;
+
+        for (int i = 1; i < n; i++)
+            for (int j = 0; j <= amount; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= coins[i]) {
+                    dp[i][j] += dp[i][j - coins[i]];
+                }
+            }
+        return dp[n - 1][amount];
     }
 }
+
+/**
+ public int change(int amount, int[] coins) {
+ int n = coins.length;
+ if (n == 0) {
+ return amount == 0 ? 1 : 0;
+ }
+
+ int[] dp = new int[amount + 1];
+ // base case
+ dp[0] = 1;
+
+ for (int i = 0; i < n; i++)
+ for (int j = 0; j <= amount; j++) {
+ // dp[i][j] = dp[i - 1][j];
+ dp[j] = dp[j];
+ if (j >= coins[i]) {
+ // dp[i][j] += dp[i][j - coins[i]];
+ dp[j] = dp[j] + dp[j - coins[i]];
+ }
+ }
+ return dp[amount];
+ }
+ */
