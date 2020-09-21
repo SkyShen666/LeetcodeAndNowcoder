b45d247e073271411c95b11e8e642943a3afa6f3
shenguanchu <543190701@qq.com>
2020-09-21 10:50:39 +0800

    刷题鸭

--- src/树/No_538Convert_BST_to_Greater_Tree_Easy.java
+++ src/树/No_538Convert_BST_to_Greater_Tree_Easy.java
@@ -7,21 +7,13 @@ public class No_538Convert_BST_to_Greater_Tree_Easy {
     private int sum = 0;
 
     public TreeNode convertBST(TreeNode root) {
-        traversal(root);
-        return root;
-    }
-
-    /**
-     * 先遍历右子树
-     * @param node
-     */
-    private void traversal(TreeNode node) {
-        if (node == null) {
-            return ;
+        if (root == null) {
+            return root;
         }
-        traversal(node.right);
-        sum += node.val;
-        node.val = sum;
-        traversal(node.left);
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
     }
 }
