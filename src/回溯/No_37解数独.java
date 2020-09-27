e5e0e991afffdbe0e87f2afa398a62de5b213267
shenguanchu <543190701@qq.com>
2020-09-27 21:38:02 +0800

    刷题鸭

--- src/回溯/No_37解数独.java
+++ src/回溯/No_37解数独.java
@@ -1,21 +1,27 @@
 package 回溯;
 import java.util.*;
 public class No_37解数独 {
-    // i：第i行，j：数字digit = j+1
-    private boolean[][] row = new boolean[9][9];
-    private boolean[][] col = new boolean[9][9];
-    private boolean[][][] block = new boolean[3][3][9];
-    private boolean valid = false;
+    // i：第i行，j：数字digit
+    private boolean[][] row;
+    private boolean[][] col;
+    private boolean[][][] block;
+    private boolean valid;
     // 存储空白格位置
-    private List<int[]> spaces = new ArrayList<>();
-
+    private List<int[]> spaces;
     public void solveSudoku(char[][] board) {
+        // j 下标从0开始，而需要存储数字1-9
+        // 对应row[1][9]: 第1行，是否存储数字9
+        row = new boolean[9][10];
+        col = new boolean[9][10];
+        block = new boolean[3][3][10];
+        valid = false;
+        spaces = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') {
                     spaces.add(new int[]{i, j});
                 } else {
-                    int digit = board[i][j] - '0' - 1;
+                    int digit = board[i][j] - '0';
                     row[i][digit] = col[j][digit] = block[i / 3][j / 3][digit] = true;
                 }
             }
@@ -31,10 +37,10 @@ public class No_37解数独 {
 
         int[] space = spaces.get(begin);
         int i = space[0], j = space[1];
-        for (int digit = 0; digit < 9 && !valid; digit++) {
+        for (int digit = 1; digit <= 9 && !valid; digit++) {
             if (!row[i][digit] && !col[j][digit] && !block[i / 3][j / 3][digit]) {
+                board[i][j] = (char)(digit + '0');
                 row[i][digit] = col[j][digit] = block[i / 3][j / 3][digit] = true;
-                board[i][j] = (char)(digit + '0' + 1);
                 dfs(board, begin + 1);
                 row[i][digit] = col[j][digit] = block[i / 3][j / 3][digit] = false;
             }
