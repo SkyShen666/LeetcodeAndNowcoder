811908d881f63b121c1297cdd9a94092d6b70d43
shenguanchu <543190701@qq.com>
2020-10-02 21:21:41 +0800

    刷题鸭

--- src/二分查找/No_81搜索旋转数组2.java
+++ src/二分查找/No_81搜索旋转数组2.java
@@ -1,27 +1,31 @@
 package 二分查找;
 
-public class No_33搜索旋转排序数组 {
-    public int search(int[] nums, int target) {
+public class No_81搜索旋转数组2 {
+    public boolean search(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
-            return -1;
+            return false;
         }
+
         int left = 0;
         int right = nums.length - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                return mid;
+                return true;
             }
-            // mid在左边
-            if (nums[mid] >= nums[left]) {
-                // target在左边
+
+            // 排除mid和left一样的情况，将区间向前搜索，[left + 1, right]
+            if (nums[mid] == nums[left]) {
+                left = left + 1;
+            } else if (nums[mid] > nums[left]) { // mid在左侧
+                // target在mid左侧
                 if (target >= nums[left] && target <= nums[mid]) {
                     right = mid - 1;
-                } else { // target在右边
+                } else {
                     left = mid + 1;
                 }
-            } else { // mid在右边
-                // target在右边
+            } else { // mid在右侧
+                // target在mid右侧
                 if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 } else {
@@ -29,7 +33,6 @@ public class No_33搜索旋转排序数组 {
                 }
             }
         }
-        return nums[left] == target ? left : -1;
+        return nums[left] == target ? true : false;
     }
 }
-
