e4f106be7b8e29303b5d88bae63745f4f799e2f8
shenguanchu <543190701@qq.com>
2020-10-19 15:33:15 +0800

    刷题鸭

---
+++ src/字符串/No_844比较含退格的字符串.java
@@ -0,0 +1,47 @@
+package 字符串;
+
+public class No_844比较含退格的字符串 {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1;
+        int j = T.length() - 1;
+        int skipS = 0;
+        int skipT = 0;
+
+        while(i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            if (i >= 0 && j >= 0) {
+                if (S.charAt(i) != T.charAt(j)) {
+                    return false;
+                }
+            } else {
+                if (i >= 0 || j >= 0) {
+                    return false;
+                }
+            }
+            i--;
+            j--;
+        }
+        return true;
+    }
+}
