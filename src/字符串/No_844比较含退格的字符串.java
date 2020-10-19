6cc8eec2df0c1e7a0b3a04638dbd65cb84e1301c
shenguanchu <543190701@qq.com>
2020-10-19 15:51:57 +0800

    刷题鸭

--- src/字符串/No_844比较含退格的字符串.java
+++ src/字符串/No_844比较含退格的字符串.java
@@ -10,19 +10,24 @@ public class No_844比较含退格的字符串 {
         int j = T.length() - 1;
         int skipS = 0;
         int skipT = 0;
-
-        while(i >= 0 || j >= 0) {
+        // 逆序遍历
+        while (i >=0 || j >= 0) {
+            // 检查目前的退格数量,并退格
             while (i >= 0) {
                 if (S.charAt(i) == '#') {
+                    // 记录退格数量
                     skipS++;
                     i--;
                 } else if (skipS > 0) {
+                    // 退格
                     skipS--;
                     i--;
                 } else {
+                    // 没有退格了,并且不需要继续退格
                     break;
                 }
             }
+
             while (j >= 0) {
                 if (T.charAt(j) == '#') {
                     skipT++;
@@ -34,15 +39,19 @@ public class No_844比较含退格的字符串 {
                     break;
                 }
             }
+
+            // 比较当前的字符
             if (i >= 0 && j >= 0) {
                 if (S.charAt(i) != T.charAt(j)) {
                     return false;
                 }
-            } else {
+            } else { // 第一个可能是退格符"#",退格符不计入比较范围
+                // 有一个字符串已经比较完,另一个还有字符没有比较完
                 if (i >= 0 || j >= 0) {
                     return false;
                 }
             }
+            // 继续比较下一个字符
             i--;
             j--;
         }
