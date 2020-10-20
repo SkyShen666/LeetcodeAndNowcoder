a2e019ac0d101fd3a8719a6d5eecf2d883bff4e5
shenguanchu <543190701@qq.com>
2020-10-20 10:16:07 +0800

    刷题鸭

---
+++ src/链表/No_143重排链表.java
@@ -0,0 +1,27 @@
+package 链表;
+
+public class No_143重排链表 {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        // 存储到list中
+        List<ListNode> list = new ArrayList<>();
+        while(head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            list.get(i).next = list.get(j);
+            i++;
+            // 偶数个会提前相遇
+            if (i == j) {
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(i).next = null;
+    }
+}
