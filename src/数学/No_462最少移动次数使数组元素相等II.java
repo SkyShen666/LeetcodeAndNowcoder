3102d466a51c2c98bb636d3eabee52710f5a548d
shenguanchu <543190701@qq.com>
2020-12-06 20:57:35 +0800

    刷题鸭

--- src/数学/No_462最少移动次数使数组元素相等II.java
+++ src/数学/No_462最少移动次数使数组元素相等II.java
@@ -1,20 +1,25 @@
 package 数学;
-
 import java.util.Arrays;
 
+// 相遇问题，移动距离最小的方式是所有元素都移动到中位数
+// 设m为中位数，若a <= m <= b, 则若想使a，b都化为m，需要的步数为(m-a) + (b - m) 即 b - a
 public class No_462最少移动次数使数组元素相等II {
-    //相遇问题，移动距离最小的方式是所有元素都移动到中位数
-    //设m为中位数，若a <= m <= b, 则若想使a，b都化为m，需要的步数为(m-a) + (b - m) 即 b - a
     public int minMoves2(int[] nums) {
-        Arrays.sort(nums);
-        int l = 0;
-        int h = nums.length - 1;
-        int moveCnt = 0;
-        while (l < h) {
-            moveCnt += nums[h] - nums[l];
-            l++;
-            h--;
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
-        return moveCnt;
+
+        Arrays.sort(nums);
+        int n = nums.length;
+        int low = 0, high = n - 1;
+        int cnt = 0;
+
+        while (low < high) {
+            cnt += nums[high] - nums[low];
+            low++;
+            high--;
+        }
+
+        return cnt;
     }
 }
