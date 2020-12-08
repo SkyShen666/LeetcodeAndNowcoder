46180c3a1b97863daa012b901ac4dbd601c24b8a
shenguanchu <543190701@qq.com>
2020-12-08 19:38:36 +0800

    刷题鸭

--- src/动态规划/No_978最长湍流子数组.java
+++ src/动态规划/No_978最长湍流子数组.java
@@ -8,25 +8,26 @@ public class No_978最长湍流子数组 {
     public int maxTurbulenceSize(int[] A) {
         int n = A.length;
         // f[i] 为 [0 ~ i - 1] 中 最后一段为“上升”的数组
-        int[] f = new int[n + 1];
+        int[] up = new int[n + 1];
         // g[i] 为 [0 ~ i - 1] 中 最后一段为“下降”的数组
-        int[] g = new int[n + 1];
+        int[] down = new int[n + 1];
 
-        f[1] = 1;
-        g[1] = 1;
+        up[1] = 1;
+        down[1] = 1;
 
         int max = 1;
         for (int i = 2; i <= n; i++) {
             // 最后一段上升
-            f[i] = A[i - 2] < A[i - 1] ? g[i - 1] + 1 : 1;
+            up[i] = A[i - 2] < A[i - 1] ? down[i - 1] + 1 : 1;
 
             // 最后一段下降
-            g[i] = A[i - 2] > A[i - 1] ? f[i - 1] + 1 : 1;
+            down[i] = A[i - 2] > A[i - 1] ? up[i - 1] + 1 : 1;
 
             // 更新最大值
-            max = Math.max(max, f[i]);
-            max = Math.max(max, g[i]);
+            max = Math.max(max, up[i]);
+            max = Math.max(max, down[i]);
         }
+
         return max;
     }
 }
