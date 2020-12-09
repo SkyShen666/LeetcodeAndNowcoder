8b6eb5ab13f21829edb52a1e60d9655fe9f719da
shenguanchu <543190701@qq.com>
2020-12-09 10:40:58 +0800

    刷题鸭

--- src/链表/No_142环形链表2.java
+++ src/链表/No_142环形链表2.java
@@ -1,5 +1,13 @@
 package 链表;
 
+// 参考题解：
+// https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/linked-list-cycle-ii-kuai-man-zhi-zhen-shuang-zhi-/
+// 1. 设入环之前有a个节点，环中有b个节点
+// 2. f = 2s （快指针走过的路程是满指针的两倍）
+// 3. f = s + nb (快慢指针相遇时，快指针比满指针刚好多走n圈)
+// 4. 推出：s = nb (慢指针已走nb步)
+// 5. 若从head节点开始走，需要走 a + nb 步能走到入环点, 此时 s 已走 nb 步，还需走 a 步即可到达入环点
+// 6. 如何知道slow刚好走a步？ 只需从head节点开始和slow指针一起一步步走，相遇时即走了a步！
 public class No_142环形链表2 {
     public ListNode detectCycle(ListNode head) {
         if (head == null) {
