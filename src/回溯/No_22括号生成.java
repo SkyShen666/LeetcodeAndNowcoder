ea18b8b0e943506d0d063da3e32ede2ef630c2b2
shenguanchu <543190701@qq.com>
2020-12-10 23:28:47 +0800

    刷题鸭

--- src/回溯/No_22括号生成.java
+++ src/回溯/No_22括号生成.java
@@ -21,35 +21,78 @@ import java.util.*;
  * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 public class No_22括号生成 {
-    List<String> res = new ArrayList<>();
+    private List<String> list;
+    private int n;
+
     public List<String> generateParenthesis(int n) {
+        list = new ArrayList<>();
         if (n == 0) {
-            return res;
+            return list;
         }
-        dfs(n, n, "");
-        return res;
+
+        this.n = n;
+        StringBuilder str = new StringBuilder();
+
+        dfs(0, 0, str);
+
+        return list;
     }
 
-    private void dfs(int left, int right, String path) {
-        // 因为每一次尝试，都使用新的字符串变量，所以无需回溯
-        // 在递归终止的时候，直接把它添加到结果集即可，注意与「力扣」第 46 题、第 39 题区分
-        if (left == 0 && right == 0) {
-            res.add(path.toString());
+    private void dfs(int left, int right, StringBuilder str) {
+        if (left == n && right == n) {
+            list.add(str.toString());
             return;
         }
 
-
-        // 剪枝（如图，左括号可以使用的个数严格大于右括号可以使用的个数，才剪枝，注意这个细节）
-        if (left < 0 || right < left) {
+        if (left > n || right > left) {
             return;
         }
 
-        if (left > 0) {
-            dfs(left - 1, right, path + "(");
+        if (left <= n) {
+            str.append("(");
+            dfs(left + 1, right, str);
+            str.deleteCharAt(str.length() - 1);
         }
-
-        if (right > 0) {
-            dfs(left, right - 1, path + ")");
+        if (right <= n) {
+            str.append(")");
+            dfs(left, right + 1, str);
+            str.deleteCharAt(str.length() - 1);
         }
     }
 }
+
+// 不使用StringBuilder，使用String
+// 可以不显示地回溯
+//
+//    List<String> res = new ArrayList<>();
+//
+//    public List<String> generateParenthesis(int n) {
+//        if (n == 0) {
+//            return res;
+//        }
+//        dfs(n, n, "");
+//        return res;
+//    }
+//
+//    private void dfs(int left, int right, String path) {
+//        // 因为每一次尝试，都使用新的字符串变量，所以无需回溯
+//        // 在递归终止的时候，直接把它添加到结果集即可，注意与「力扣」第 46 题、第 39 题区分
+//        if (left == 0 && right == 0) {
+//            res.add(path.toString());
+//            return;
+//        }
+//
+//
+//        // 剪枝（如图，左括号可以使用的个数严格大于右括号可以使用的个数，才剪枝，注意这个细节）
+//        if (left < 0 || right < left) {
+//            return;
+//        }
+//
+//        if (left > 0) {
+//            dfs(left - 1, right, path + "(");
+//        }
+//
+//        if (right > 0) {
+//            dfs(left, right - 1, path + ")");
+//        }
+//    }
--- src/数学/No_134加油站.java
+++ src/数学/No_134加油站.java
@@ -1,20 +1,24 @@
 package 数学;
-// 参考题解:https://leetcode-cn.com/problems/gas-station/solution/shi-yong-tu-de-si-xiang-fen-xi-gai-wen-ti-by-cyayc/
+//  参考题解:
+//  https://leetcode-cn.com/problems/gas-station/solution/shi-yong-tu-de-si-xiang-fen-xi-gai-wen-ti-by-cyayc/
 //  1. 首先判断总gas能不能大于等于总cost，如果总gas不够，一切都白搭对吧；
 //  2. 有序列表再就是找总（gas-cost）的最低点，不管正负（当然如果最低点都是正的话那肯定能跑完了）；
 //  3. 找到最低点后，如果有解，那么解就是最低点的下一个点，因为总（gas-cost）是大于等于0的，所以前面损失的gas我从最低点下一个点开始都会拿回来！别管后面的趋势是先加后减还是先减后加，最终结果我是能填平前面的坑的。
 public class No_134加油站 {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int min = Integer.MAX_VALUE;
-        int res = -1;
-        int remain = 0;
-        for (int i = 0; i < gas.length; i++) {
+        int n = gas.length;
+        int min = Integer.MAX_VALUE; // 记录 gas - cost 最低点
+        int remain = 0; // 记录还剩多少油
+        int ret = 0; // 最低点的下一个索引，是出发点
+
+        for (int i = 0; i < n; i++) {
             remain += gas[i] - cost[i];
             if (min > remain) {
-                res = i;
                 min = remain;
+                ret = i + 1;
             }
         }
-        return remain >= 0 ? (res + 1) % gas.length : -1;
+
+        return remain >= 0 ? ret % n : -1;
     }
 }
