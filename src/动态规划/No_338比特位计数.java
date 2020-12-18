ccb53f3b6ed03dd4b0016b7ee545aefaf84cd1d6
shenguanchu <543190701@qq.com>
2020-12-18 23:36:00 +0800

    刷题鸭

--- src/动态规划/No_338比特位计数.java
+++ src/动态规划/No_338比特位计数.java
@@ -1,5 +1,7 @@
 package 动态规划;
 
+// 参考答案
+// https://leetcode-cn.com/problems/counting-bits/solution/hen-qing-xi-de-si-lu-by-duadua/
 public class No_338比特位计数 {
     public int[] countBits(int num) {
         int[] dp = new int[num + 1];
---
+++ src/数组与矩阵/No_48旋转图像.java
@@ -0,0 +1,25 @@
+package 数组与矩阵;
+
+public class No_48旋转图像 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // 1.转置矩阵
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+
+        // 2.颠倒每一行
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+}
