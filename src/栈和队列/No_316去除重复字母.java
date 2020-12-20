5349961299e6d101462787ad181d9c85ef730983
shenguanchu <543190701@qq.com>
2020-12-20 10:04:24 +0800

    刷题鸭

---
+++ src/栈和队列/No_316去除重复字母.java
@@ -0,0 +1,48 @@
+package 栈和队列;
+import java.util.*;
+// 单调栈
+// 参考题解：
+// https://leetcode-cn.com/problems/remove-duplicate-letters/solution/zhan-by-liweiwei1419/
+public class No_316去除重复字母 {
+    public String removeDuplicateLetters(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+
+        int n = s.length();
+        char[] charArray = s.toCharArray();
+        int[] lastIndex = new int[26];
+        StringBuilder str = new StringBuilder();
+
+        // 1.统计每个字母最后出现的位置
+        for (int i = 0; i < n; i++) {
+            lastIndex[charArray[i] - 'a'] = i;
+        }
+
+        Deque<Character> stack = new LinkedList<>();
+        boolean[] visited = new boolean[26];
+
+        // 2. 通过遍历字符串，使用单调栈维护去除重复字母的字符串。
+        for (int i = 0; i < n; i++) {
+            char c = charArray[i];
+            if (visited[c - 'a']) {
+                continue;
+            }
+
+            while (!stack.isEmpty() && c < stack.peekLast() && lastIndex[stack.peekLast() - 'a'] > i) {
+                char top = stack.removeLast();
+                // 注意维护visited数组
+                visited[top - 'a'] = false;
+            }
+
+            stack.addLast(c);
+            visited[c - 'a'] = true;
+        }
+
+        for (char c : stack) {
+            str.append(c);
+        }
+
+        return str.toString();
+    }
+}
