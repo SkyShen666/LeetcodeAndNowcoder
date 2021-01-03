d931d097e3f60b27f868e29e023a9eeb9e64a8f9
shenguanchu <543190701@qq.com>
2021-01-03 22:10:41 +0800

    刷题

--- src/每日一题/剑指_39数组中出现次数超过一半的数字.java
+++ src/每日一题/剑指_39数组中出现次数超过一半的数字.java
@@ -1,12 +1,15 @@
 package 每日一题;
 
 /**
- * 同leetcode169
- * 投票法：
+ * 同leetcode169、
+ *
+ * 解法1： 快排
+ *
+ * 解法2： 投票法
  *  1、遇到相同的就投一票，不同的就减一票
  *  2、当票数为0，就更新 “众数”
  */
-public class 数组中出现次数超过一半的数字 {
+public class 剑指_39数组中出现次数超过一半的数字 {
     public int majorityElement(int[] nums) {
         int cnt = 0;
         int majority = nums[0];
