225a79030df351db3280b9f6303e9816c473ffb2
shenguanchu <543190701@qq.com>
2021-03-25 16:21:04 +0800

    刷题

--- src/每日一题/剑指48_最长不含重复字符的子字符串.java
+++ src/每日一题/剑指48_最长不含重复字符的子字符串.java
@@ -8,22 +8,19 @@ import java.util.*;
  */
 public class 剑指48_最长不含重复字符的子字符串 {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>(); // HashMap存储遍历过的元素
+        int maxLen = 0; // 记录滑动窗口最大长度
         int begin = 0; // 滑动窗口最左侧下标
-        int maxLen = 0;
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (map.containsKey(c)) {
-                // 如：abccbade 第二个窗口cba，到b时(map中已放置b，在第一个窗口abc时)，不需要更换begin, 不被已放入map中，旧的元素影响。
+            if (map.containsKey(c)) { // 是否更新滑动窗口起点begin的位置
+                // 不更新begin如：abccbade 第二个窗口cba，到b时(map中已放置b，在第一个窗口abc时)，不需要更换begin, 不被已放入map中的、旧的元素影响。
                 begin = Math.max(begin, map.get(c) + 1);
             }
-            // 更新位置，和滑动窗口长度
+            // 在HashMap中更新此次遍历到的元素
             map.put(c, i);
+            // 更新 滑动窗口长度
             maxLen = Math.max(maxLen, i - begin + 1);
         }
 
