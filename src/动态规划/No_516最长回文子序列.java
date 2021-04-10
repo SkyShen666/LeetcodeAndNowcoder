c57adc46a2b4956809c42ee6fc13085a33a59baf
shenguanchu <543190701@qq.com>
2021-04-10 15:50:06 +0800

    刷题

--- src/动态规划/No_516最长回文子序列.java
+++ src/动态规划/No_516最长回文子序列.java
@@ -5,23 +5,19 @@ package 动态规划;
 // https://leetcode-cn.com/problems/longest-palindromic-subsequence/solution/dong-tai-gui-hua-si-yao-su-by-a380922457-3/
 public class No_516最长回文子序列 {
     public int longestPalindromeSubseq(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-
-        int len = s.length();
+        int n = s.length();
         // dp[i][j] 表示 s 的第 i 个字符到第 j 个字符组成的子串中，最长的回文序列长度是多少
-        int[][] dp = new int[len][len];
-        for (int i = 0; i < len; i++) {
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
             dp[i][i] = 1;
         }
 
         // 然后注意遍历顺序
-        // i 从最后一个字符开始往前遍历
+        // i 从倒数第二个字符开始往前遍历
         // j 从 i + 1 开始往后遍历
         // 这样可以保证每个子问题都已经算好了。
-        for (int i = len - 2; i >= 0; i--) {
-            for (int j = i + 1; j < len; j++) {
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
                 if (s.charAt(i) == s.charAt(j)) {
                     dp[i][j] = dp[i + 1][j - 1] + 2;
                 } else {
@@ -30,6 +26,6 @@ public class No_516最长回文子序列 {
             }
         }
 
-        return dp[0][len - 1];
+        return dp[0][n - 1];
     }
 }
--- src/字符串/No_409最长回文串.java
+++ src/字符串/No_409最长回文串.java
@@ -1,8 +1,13 @@
 package 字符串;
 
-// int[] 计数法
-// 参考题解：
-// https://leetcode-cn.com/problems/longest-palindrome/solution/javade-2chong-shi-xian-fang-fa-by-sweetiee/
+/**
+ * 注意：是构造！通过这些字母构造成的最长的回文串。
+ * 解题方法：(计数法)
+ *      int[] cntArray = new int[58];
+ *      刚开始不知道为什么要58的数组，大小写字母只有52个，后来查阅了一下ASCII编码表，原来大小写字母之间有好几个特殊字符。
+ * 参考题解：
+ * https://leetcode-cn.com/problems/longest-palindrome/solution/javade-2chong-shi-xian-fang-fa-by-sweetiee/
+ */
 public class No_409最长回文串 {
     public int longestPalindrome(String s) {
         if (s == null || s.length() == 0) {
@@ -10,14 +15,13 @@ public class No_409最长回文串 {
         }
 
         int max = 0;
-        int[] cnt = new int[58];
+        int[] cntArray = new int[58];
         for (char c : s.toCharArray()) {
-            cnt[c - 'A']++;
+            cntArray[c - 'A']++;
         }
-
-        for (int n : cnt) {
-            // 把出现奇数次和偶数次看做一样，都是偶数次。(字符出现的次数最多用偶数次)
-            max = max + (n / 2) * 2;
+        for (int cnt : cntArray) {
+            // 把出现奇数次和偶数次看做一样，都是偶数次。(字符出现的次数最多用偶数次)，若为奇数次：1 / 2 = 0, 3 / 2 = 1...
+            max += (cnt / 2) * 2;
         }
 
         // 如果最终的长度小于原字符串的长度，说明里面某个字符出现了奇数次，那么那个字符可以放在回文串的中间，所以额外再加一。
